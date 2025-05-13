c955d76da7c9b1ae9111598ca66deea7841c1707
Akanksha-Bathla <akankshabathla.18.22.26@gmail.com>
2025-05-13 13:19:04 +0530

    mongoDBConnection established

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import { app } from "./app.js";
+import connectDB from "./db/index.js";
 
 dotenv.config({
     path:"./.env"
@@ -7,6 +8,13 @@ dotenv.config({
 
 const PORT = process.env.PORT || 8001
 
-app.listen(8000,()=>{
-    console.log(`Server is running on port ${PORT}`)
+
+connectDB()
+.then(()=>{
+    app.listen(8000,()=>{
+        console.log(`Server is running on port ${PORT}`)
+    })
+})
+.catch((err) => {
+    console.log("Mongodb connection error", err)
 })
