cc31d91ecf751099c3aafddcf211bfc7b4bd7619
Akanksha-Bathla <akankshabathla.18.22.26@gmail.com>
2025-05-17 17:04:49 +0530

    setup for file handling is done using multer ad cloudinary

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from "express"
+import cookieParser from "cookie-parser"
 import cors from "cors"
 
 const app = express()
@@ -15,6 +16,7 @@ app.use(
 app.use(express.json({limit: "16kb"})) 
 app.use(express.urlencoded({ extended: true, limit: "16kb"}))
 app.use(express.static("public"))
+app.use(cookieParser())
 
 // import routes
 import healthcheckRouter from "./routes/healthcheck.route.js"
